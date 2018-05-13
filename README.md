8465486a09d3b31ccc496ad248cac4a078563a00
trizzle21 <tdross21@gmail.com>
2018-05-13 11:47:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Chess League v1
---
+++ web/core/api/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class League(models.Model):
+    name = models.CharField(max_length=36)
+
